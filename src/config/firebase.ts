a8623b9fba56173c68630344150cf1ab7a699e96
KishiTheMechanic <s.kishi@elsoul.nl>
2023-06-28 04:31:00 +0200

    firebsae config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyCOHcxSDUu6G-EfI___yi3T3b2WHO-Zsw8',
-  authDomain: 'skeet-framework.firebaseapp.com',
-  projectId: 'skeet-framework',
-  storageBucket: 'skeet-framework.appspot.com',
-  messagingSenderId: '213528932539',
-  appId: '1:213528932539:web:a2b591980dc6e4dc6ab99e',
-  measurementId: 'G-3L24HB4PBY',
+  apiKey: 'AIzaSyATSdJHATWU04LKfrgzen1sFCwwsl93D8U',
+  authDomain: 'skeet-next.firebaseapp.com',
+  projectId: 'skeet-next',
+  storageBucket: 'skeet-next.appspot.com',
+  messagingSenderId: '1037692178989',
+  appId: '1:1037692178989:web:5bb909c7b0cef7600e9ea4',
+  measurementId: 'G-8BZJ1C7RW1',
 }
 
 export default firebaseConfig
