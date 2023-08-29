8d8c9c0c847e92796dcf1fb1e405ffa213835026
KishiTheMechanic <s.kishi@elsoul.nl>
2023-08-29 20:32:20 +0200

    update title

--- functions/skeet/src/routings/http/addVertexMessage.ts
+++ functions/skeet/src/routings/http/addVertexMessage.ts
@@ -27,14 +27,17 @@ export const addVertexMessage = onRequest(
         chatRoomPath,
         req.body.vertexChatRoomId,
       )
-      console.log(vertexChatRoomData)
       const vertexExampleData = vertexChatRoomData.examples
 
       // Send to VertexAI
       const response = await sendToVertexAI(
-        vertexChatRoomData,
+        {
+          id: req.body.vertexChatRoomId,
+          ...vertexChatRoomData,
+        },
         vertexExampleData,
         req.body.content,
+        chatRoomPath,
       )
 
       // Add User Message to VertexChatRoomMessage
