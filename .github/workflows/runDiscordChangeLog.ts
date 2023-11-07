c081d5da3843fe31efd2e947e6fb539b67493cb2
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-07 19:38:49 +0100

    update release actions

--- .github/workflows/runDiscordChangeLog.ts
+++ .github/workflows/runDiscordChangeLog.ts
@@ -2,26 +2,26 @@ import { discordChangeLog } from '@skeet-framework/discord-utils'
 import { dotenv } from '@skeet-framework/utils'
 dotenv.config()
 
-const REPO_NAME = 'elsoul/skeet-next'
 type ProjectType = 'labo' | 'epics'
 
-const run = async (project: ProjectType) => {
+const run = async (project: ProjectType, repoName: string) => {
   if (project === 'labo') {
     console.log('labo')
     const token = process.env.DISCORD_TOKEN_LABO || ''
     const channelId = process.env.LABO_SKEET_CHANNEL_ID || ''
-    await discordChangeLog(token, REPO_NAME, [channelId])
+    await discordChangeLog(token, repoName, [channelId])
   } else if (project === 'epics') {
     console.log('epics')
     const token = process.env.DISCORD_TOKEN || ''
     const channelId = process.env.DISCORD_CHANNEL_ID || ''
     const channelIdJA = process.env.DISCORD_CHANNEL_ID_JA || ''
-    await discordChangeLog(token, REPO_NAME, [channelId])
-    await discordChangeLog(token, REPO_NAME, [channelIdJA], 'ja')
+    await discordChangeLog(token, repoName, [channelId])
+    await discordChangeLog(token, repoName, [channelIdJA], 'ja')
   } else {
     console.log('invalid project name')
   }
 }
 
 const project = process.argv[2] as ProjectType
-void run(project)
+const repoArg = process.argv[3] || ''
+void run(project, repoArg)
